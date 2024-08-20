58ff0381ed10384c171d8e5e0cfc1aa36c39a77b
salarp-dev <>
2024-08-20 21:58:43 +0300

    gg

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def oxapay_webhook():
         user_chat_id = extract_user_chat_id_from_order(order_id)
         product_details = get_product_details(order_id)
         
-        # Send a message to the user via Telegram with pickup details
+        # Send a message \\to the user via Telegram with pickup details
         send_telegram_message(user_chat_id, f"Your payment was successful! Here are your pickup details: {product_details}")
     
     return 'Webhook received', 200
